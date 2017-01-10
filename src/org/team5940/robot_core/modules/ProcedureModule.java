9ee654a4a1ef487fe70a80c02022e6bca2bf0e0a
bertturtle <mbentley124@gmail.com>
2017-01-09 17:24:50 -0800

    Added Javadocs

--- src/org/team5940/robot_core/modules/ProcedureModule.java
+++ src/org/team5940/robot_core/modules/ProcedureModule.java
@@ -1,15 +1,55 @@
 package org.team5940.robot_core.modules;
 
 public interface ProcedureModule extends Module {
-
+	
+	/**
+	 * The states the procedure can be in
+	 */
 	public enum ProcedureState {
-		RUNNING, NOT_STARTED, FINISHED, INTERRUPTED, ERROR
+		
+		/**
+		 * Procedure is running
+		 */
+		RUNNING, 
+		
+		/**
+		 * Procedure has not been started yet
+		 */
+		NOT_STARTED, 
+		
+		/**
+		 * Procedure is finished
+		 */
+		FINISHED, 
+		
+		/**
+		 * Procedure was interrupted prematurely
+		 */
+		INTERRUPTED, 
+		
+		/**
+		 * Procedure had an error
+		 */
+		ERROR
+		
 	}
-
+	
+	/**
+	 * Starts the procedure
+	 * @return if it was able to start it
+	 */
 	public boolean start();
 
-	public boolean interrupt();
+	/**
+	 * Stops the procedure prematurely
+	 * @return if it was currently running
+	 */
+	public boolean stop();
 
+	/**
+	 * @return the current state
+	 * @see ProcedureState
+	 */
 	public ProcedureState getState();
 
 }
