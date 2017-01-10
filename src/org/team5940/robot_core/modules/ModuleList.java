e747282b8e948bea40bfe01072ef888676535c3b
David Boles <david.e.boles22@gmail.com>
2017-01-10 13:38:41 -0800

    Created ModuleList

---
+++ src/org/team5940/robot_core/modules/ModuleList.java
@@ -0,0 +1,15 @@
+package org.team5940.robot_core.modules;
+
+import java.util.Hashtable;
+
+public class ModuleList extends Hashtable<String, Module> {
+	private static final long serialVersionUID = 1L;
+
+	public ModuleList getDirectSubModulesImplementing(Class<? extends Module> parent) {
+		return null;//TODO do it
+	}
+	
+	public ModuleList getAllSubModulesImplementing(Class<? extends Module> parent) {
+		return null;//TODO do it
+	}
+}
