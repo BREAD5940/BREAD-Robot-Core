454d53f635fd49afb8d4fb74e68873505ffed7b7
David Boles <david.e.boles22@gmail.com>
2017-01-10 14:47:50 -0800

    Partially completed ModuleHashTable

--- src/org/team5940/robot_core/modules/ModuleHashTable.java
+++ src/org/team5940/robot_core/modules/ModuleHashTable.java
@@ -1,17 +1,29 @@
 package org.team5940.robot_core.modules;
 
-import java.util.AbstractList;
+import java.lang.reflect.Array;
 import java.util.Hashtable;
 
 public class ModuleHashTable<T extends Module> extends Hashtable<String, T> {//TODO add generics so can specify extention of Module
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Creates a new ModuleList with modules and the keys them.getName().
-	 * @param modules The modules in this ModuleList.
+	 * Creates a new, empty ModuleList.
+	 * @param initModules The modules in this ModuleList.
 	 */
-	public ModuleHashTable(AbstractList<T> modules) {
-		//TODO
+	public ModuleHashTable() {
+		super();
+	}
+	
+	/**
+	 * Creates a new ModuleList with initModules and the keys them.getName().
+	 * @param initModules The modules in this ModuleList.
+	 */
+	public ModuleHashTable(T[] initModules) {
+		super();
+		for(int i = 0; i < initModules.length; i++) {
+			T module = initModules[i];
+			this.put(module.getName(), module);
+		}
 	}
 	
 	/**
@@ -20,7 +32,18 @@ public class ModuleHashTable<T extends Module> extends Hashtable<String, T> {//T
 	 * @return A ModuleList containing direct submodules that are assignable from parent.
 	 */
 	public ModuleHashTable<T> getDirectSubModulesAssignableFrom(Class<? extends Module> parent) {
-		return null;//TODO do it
+		ModuleHashTable<T> out = new ModuleHashTable<T>();
+		
+		@SuppressWarnings("unchecked")
+        T[] modules = (T[]) Array.newInstance(Module.class, 0);
+		modules = this.values().toArray(modules);
+		
+		for(int i = 0; i < modules.length; i++) {
+			T module = modules[i];
+			this.put(module.getName(), module);
+		}
+		
+		return out;
 	}
 	
 	/**
@@ -29,6 +52,19 @@ public class ModuleHashTable<T extends Module> extends Hashtable<String, T> {//T
 	 * @return A ModuleList containing all submodules that are assignable from parent.
 	 */
 	public ModuleHashTable<T> getAllSubModulesAssignableFrom(Class<? extends Module> parent) {
-		return null;//TODO do it
+		ModuleHashTable<T> out = new ModuleHashTable<T>();
+		
+		@SuppressWarnings("unchecked")
+        T[] modules = (T[]) Array.newInstance(Module.class, 0);
+		modules = this.values().toArray(modules);
+		
+		for(int i = 0; i < modules.length; i++) {
+			T module = modules[i];
+			this.put(module.getName(), module);
+			//TODO add submodules
+			
+		}
+		
+		return out;
 	}
 }
