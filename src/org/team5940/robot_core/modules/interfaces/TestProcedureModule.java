28d3c57f37495ead5ebdf1556d02a5e3e58994e4
RaianLOL <rycen618@gmail.com>
2017-01-11 20:31:00 -0800

    Moved enum to TestableModule

--- src/org/team5940/robot_core/modules/interfaces/TestProcedureModule.java
+++ src/org/team5940/robot_core/modules/interfaces/TestProcedureModule.java
@@ -1,35 +1,8 @@
 package org.team5940.robot_core.modules.interfaces;
 
-import org.team5940.robot_core.modules.Module;
+import org.team5940.robot_core.modules.interfaces.TestableModule.TestingState;
 
-public interface TestProcedureModule extends Module {
-	
-	/**
-	 * States that the test can be in.
-	 */
-	public enum TestingState {
-		 
-		/**
-		 * The test is running.
-		 */
-		RUNNING,
-		
-		/**
-		 * The test is successful.
-		 */
-		
-		SUCCESSFUL,
-		
-		/**
-		 * The test failed.
-		 */
-		FAILED,
-		
-		/**
-		 * There is in error in the test.
-		 */
-		ERROR
-	}
+public interface TestProcedureModule extends ProcedureModule {
 	
 	/**
 	 * Returns the state that the test is in.
--- src/org/team5940/robot_core/modules/interfaces/TestableModule.java
+++ src/org/team5940/robot_core/modules/interfaces/TestableModule.java
@@ -5,6 +5,33 @@ import org.team5940.robot_core.modules.Module;
 public interface TestableModule extends Module {
 	
 	/**
+	 * States that the test can be in.
+	 */
+	public enum TestingState {
+		 
+		/**
+		 * The test is running.
+		 */
+		RUNNING,
+		
+		/**
+		 * The test is successful.
+		 */
+		
+		SUCCESSFUL,
+		
+		/**
+		 * The test failed.
+		 */
+		FAILED,
+		
+		/**
+		 * There is in error in the test.
+		 */
+		ERROR
+	}
+	
+	/**
 	 * Runs the test and returns true if the test runs.
 	 * @return A true or false statement on if the test runs or not.
 	 */
