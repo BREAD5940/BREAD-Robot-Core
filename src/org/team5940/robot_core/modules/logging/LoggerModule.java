24aae5e64fa606d9a0c74a3b6f263946e55f7e7f
david476 <david.e.boles22@gmail.com>
2017-01-24 18:25:28 -0800

    LoggerModule: Added Object[] as content methods. (#45)

--- src/org/team5940/robot_core/modules/logging/LoggerModule.java
+++ src/org/team5940/robot_core/modules/logging/LoggerModule.java
@@ -38,6 +38,15 @@ public interface LoggerModule extends Module {
 	public void log(Module module,Object content) throws IllegalArgumentException;
 	
 	/**
+	 * Logs the Object content, marking that it comes from Module module.
+	 * @throws IllegalArgumentExecption if any of the inputs == null.
+	 * @param module The Module that is logging.
+	 * @param content The content of the log. Will always be an object.
+	 */
+	
+	public void log(Module module,Object[] content) throws IllegalArgumentException;
+	
+	/**
 	 * Logs the String log as an error, marking that it comes from Module module. This is used to distinguish whether a logged event is not supposed to happen. 
 	 * @throws IllegalArgumentExecption if any of the inputs == null.
 	 * @param module The Module that is logging.
@@ -55,6 +64,16 @@ public interface LoggerModule extends Module {
 	 */
 	
 	public void error(Module module,String title,Object content) throws IllegalArgumentException;
+	
+	/**
+	 * Logs the Object content as an error, with the title as String title, marking that it comes from Module module. This is used to distinguish whether a logged event is not supposed to happen.
+	 * @throws IllegalArgumentExecption if any of the inputs == null.
+	 * @param module The Module that is logging.
+	 * @param title The title of the log.
+	 * @param content The content of the log. Will always be an object.
+	 */
+	
+	public void error(Module module,String title,Object[] content) throws IllegalArgumentException;
 	
 	/**
 	 * Logs the Object content as an error, marking that it comes from Module module. This is used to distinguish whether a logged event is not supposed to happen.
