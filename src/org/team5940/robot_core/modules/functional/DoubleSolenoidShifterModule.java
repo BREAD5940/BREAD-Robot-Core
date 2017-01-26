1bd41739ba0364607ae1d9a813510e7012de27d0
a <a@a-ThinkPad-X60s>
2017-01-25 19:41:58 -0800

    Made edits David wants radians

---
+++ src/org/team5940/robot_core/modules/functional/DoubleSolenoidShifterModule.java
@@ -0,0 +1,95 @@
+package org.team5940.robot_core.modules.functional;
+
+import org.team5940.robot_core.modules.Module;
+import org.team5940.robot_core.modules.ModuleHashTable;
+import org.team5940.robot_core.modules.SimpleModule;
+import org.team5940.robot_core.modules.logging.LoggerModule;
+import org.team5940.robot_core.modules.ownable.SimpleOwnableModule;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+
+public class DoubleSolenoidShifterModule extends SimpleOwnableModule implements ShifterModule{
+	
+	private DoubleSolenoid shifty_shifter;
+	private boolean Fowardishigh;
+	private short currentshift=0;
+	
+	public DoubleSolenoidShifterModule(String name, LoggerModule logger,int highGear, int lowGear, boolean fishigh)
+			throws IllegalArgumentException {
+		super(name, new ModuleHashTable<>(), logger);
+		this.shifty_shifter= new DoubleSolenoid(highGear,lowGear);
+		this.Fowardishigh=fishigh;
+		gear0();
+	}
+
+	@Override
+	public boolean isOwnedBy(Thread t) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean acquireOwnershipFor(Thread t, boolean force) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void relinquishOwnershipFor(Thread t) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initialize() {
+		
+	}
+
+	@Override
+	public void shutDown() {
+		setShift(0);
+	}
+	
+	@Override
+	public void setShift(int gear) throws IllegalArgumentException {
+		switch(gear){
+		case 0: gear0();
+				break;
+		case 1:
+			gear1();
+			break;
+		}
+	}
+
+	@Override
+	public int getShift() {
+		return currentshift;
+	}
+
+	@Override
+	public int getNumberOfGears() {
+		return 2;
+	}
+	private void gear0(){
+		if(Fowardishigh){
+			this.shifty_shifter.set(kFoward);
+			
+		}
+		else{
+			this.shifty_shifter.set(kReverse);
+		}
+		currentshift=0;
+	}
+	private void gear1(){
+		if(Fowardishigh){
+			this.shifty_shifter.set(kReverse);
+			
+		}
+		else{
+			this.shifty_shifter.set(kFoward);
+		}
+		currentshift=0;
+	}
+
+}
