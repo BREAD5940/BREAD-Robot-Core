7e9096eb0f2855f3a1771238b6fa5356e022101b
David Boles <david.e.boles22@gmail.com>
2017-01-25 19:40:07 -0800

    win_build: fixed

--- win_build.bat
+++ win_build.bat
@@ -1,4 +1,4 @@
 set /p JDKDIR=<jdkbindir.txt
 cd bin
-"%JDKDIR%\jar" cvf "%UserProfile%\wpilib\user\java\lib\BREAD-Robot-Core.jar"
+"%JDKDIR%\jar" cvf "%UserProfile%\wpilib\user\java\lib\BREAD-Robot-Core.jar" *
 pause
