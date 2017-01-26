266ffab797d578cccdab766236bca7f8ea0dab22
nsturgeon19 <nsturgeon19@dtechhs.org>
2017-01-25 17:24:22 -0800

    Added the selector module (#48)
    * Added the selector module
    
    * Fixed some Javadocs

---
+++ src/org/team5940/robot_core/modules/functional/SelectorModule.java
@@ -0,0 +1,22 @@
+package org.team5940.robot_core.modules.functional;
+
+import org.team5940.robot_core.modules.Module;
+/**
+ * A Module that can select between multiple states.
+ * @author Noah Sturgeon
+ *
+ */
+public interface SelectorModule extends Module {
+
+	/**
+	 * Gets the number of states this module has.
+	 * @return The number of states.
+	 */
+	public int getNumOfStates ();
+	
+	/**
+	 * Gets the current state of this module.
+	 * @return The current state of the module. Between 0 inclusive and getNumOfStates inclusive.
+	 */
+	public int currentState ();
+}
