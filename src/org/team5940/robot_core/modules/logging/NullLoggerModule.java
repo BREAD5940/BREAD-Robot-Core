177136f2f6a8c2ddc87b91ccd89df96727673041
David Boles <david.e.boles22@gmail.com>
2017-01-28 11:18:59 -0800

    NullLoggerModule: Simplified construction

--- src/org/team5940/robot_core/modules/logging/NullLoggerModule.java
+++ src/org/team5940/robot_core/modules/logging/NullLoggerModule.java
@@ -9,41 +9,16 @@ import org.team5940.robot_core.modules.ModuleHashTable;
  *
  */
 public class NullLoggerModule implements LoggerModule {
-	/**
-	 * Stores the name of this Module.
-	 */
-	private final String name;
-	
-	/**
-	 * Stores the submodules of this Module.
-	 */
-	private final ModuleHashTable<Module> subModules;
-	
-	/**
-	 * Creates a new SimpleModule with given arguments.
-	 * @param name The name of this Module.
-	 * @param subModules The submodules of this Module.
-	 * @throws IllegalArgumentException if any argument is null.
-	 */
-	public NullLoggerModule(String name, ModuleHashTable<Module> subModules) throws IllegalArgumentException {
-		if(name == null || subModules == null) {
-			throw new IllegalArgumentException("Argument null!");
-		}
-		this.name = name;
-		this.subModules = subModules;
-	}
 	
 	@Override
 	public String getName() {
-		return this.name;
+		return "null_logger";
 	}
 	
 
 	@Override
 	public ModuleHashTable<? extends Module> getSubModules() {
-		ModuleHashTable<Module> out = new ModuleHashTable<Module>();
-		out.putAll(this.subModules);
-		return out;
+		return new ModuleHashTable<Module>();
 	}
 
 	@Override
