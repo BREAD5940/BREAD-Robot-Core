c735158ea1e2cff8c972ec46a222c9048596f5cc
david476 <david.e.boles22@gmail.com>
2017-03-02 16:13:56 -0800

    Improved MotorSet testing.

--- src/org/team5940/robot_core/modules/actuators/motor_sets/TestableCANTalonMotorSetModule.java
+++ src/org/team5940/robot_core/modules/actuators/motor_sets/TestableCANTalonMotorSetModule.java
@@ -1,8 +1,6 @@
 package org.team5940.robot_core.modules.actuators.motor_sets;
 
 import org.team5940.robot_core.modules.logging.LoggerModule;
-import org.team5940.robot_core.modules.testing.TestableModule;
-import org.team5940.robot_core.modules.testing.communication.TestCommunicationModule;
 
 import com.ctre.CANTalon;
 
@@ -11,7 +9,7 @@ import com.ctre.CANTalon;
  * @author David Boles
  *
  */
-public class TestableCANTalonMotorSetModule extends CANTalonMotorSetModule implements TestableModule {
+public class TestableCANTalonMotorSetModule extends CANTalonMotorSetModule implements TestableMotorSetModule {
 	
 	/**
 	 * Initializes a new TestableCANTalonMotorSetModule.
@@ -38,46 +36,6 @@ public class TestableCANTalonMotorSetModule extends CANTalonMotorSetModule imple
 			throws IllegalArgumentException {
 		super(name, logger, talon);
 		this.logger.logInitialization(this, TestableCANTalonMotorSetModule.class);
-	}
-
-	@Override
-	public TestStatus runTest(TestCommunicationModule comms) throws IllegalArgumentException {
-		try {
-			//FORWARD
-			this.setMotorSpeed(0.15);
-			if(this.getSetMotorSpeed() != 0.15d) {
-				this.setMotorSpeed(0);
-				this.getModuleLogger().logTestFailed(this, "Wrong Set Forward Speed (" + this.getSetMotorSpeed() + ")");
-				return TestStatus.FAILED;
-			}
-			if(!comms.promptBoolean("Is " + this.getModuleName() + " moving forward?")) {
-				this.setMotorSpeed(0);
-				this.getModuleLogger().logTestFailed(this, "Not Moving Forward");
-				return TestStatus.FAILED;
-			}
-			
-			//BACKWARD
-			this.setMotorSpeed(-0.15);
-			if(this.getSetMotorSpeed() != -0.15d) {
-				this.setMotorSpeed(0);
-				this.getModuleLogger().logTestFailed(this, "Wrong Set Forward Speed (" + this.getSetMotorSpeed() + ")");
-				return TestStatus.FAILED;
-			}
-			if(!comms.promptBoolean("Is " + this.getModuleName() + " moving backward?")) {
-				this.setMotorSpeed(0);
-				this.getModuleLogger().logTestFailed(this, "Not Moving Backward");
-				return TestStatus.FAILED;
-			}
-			
-			//RESET
-			this.setMotorSpeed(0);
-			this.getModuleLogger().logTestPassed(this);
-			return TestStatus.PASSED;
-		}catch(Exception e) {
-			this.getModuleLogger().logTestErrored(this, e);
-			return TestStatus.ERRORED;
-		}
-		
 	}
 
 }
