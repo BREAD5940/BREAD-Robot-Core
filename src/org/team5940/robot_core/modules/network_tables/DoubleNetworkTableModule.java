e84bb2fdbd45447f0b9800f6c9b6d64a20f2ee4c
Michael Bentley <mbentley124@gmail.com>
2017-03-17 19:23:27 -0700

    Added network table module

---
+++ src/org/team5940/robot_core/modules/network_tables/DoubleNetworkTableModule.java
@@ -0,0 +1,9 @@
+package org.team5940.robot_core.modules.network_tables;
+
+import org.team5940.robot_core.modules.Module;
+
+public interface DoubleNetworkTableModule extends Module {
+	
+	public double[] getDoubles();
+	
+}
